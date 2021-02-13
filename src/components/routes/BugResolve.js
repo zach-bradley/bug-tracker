bcf1c8e8234bbf18575accc488089d13cac826a1
Zach Bradley <zachbradleydev@gmail.com>
2021-02-13 14:36:36 -0500

    created inital files for components

---
+++ src/components/routes/BugResolve.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BugResolve() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BugResolve
---
+++ src/components/routes/BugSubmit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BugSubmit() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BugSubmit
